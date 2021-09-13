1eedb32f271660ec2225160715270e4526aa590e
IBST-yoonSangHyeon <47581425+IBST-yoonSangHyeon@users.noreply.github.com>
2021-09-13 20:30:07 +0900

    express-validator 모듈 생성 및 사용

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,10 +1,95 @@
 const express = require('express');
 const router = express.Router();
 const commonModule = require('./common/module'); // require사용시 expor
+const pool = require('../../lib/mysql2Pool');
 require('dotenv').config();
+const { body, validationResult } = require('express-validator'); // 유효성 검사
 
-
-
+/**
+ * @swagger
+ * /api/user/register:
+ *  post:
+ *      tags: 
+ *       - user
+ *      summary: "회원가입"
+ *      description: "회원가입"
+ *      produces: 
+ *      - "application/json"
+ *      parameters:
+ *      - name : "mb_id"
+ *        in: "formData"
+ *        description: "회원아이디"
+ *        required: ture
+ *        type: "string"
+ *      - name : "mb_password"
+ *        in: "formData"
+ *        description: "회원 비밀번호"
+ *        required: true
+ *        type: "string"
+ *      - name : "mb_email"
+ *        in: "formData"
+ *        description: "이메일"
+ *        required: true
+ *        type: "string"
+ *      - name : "mb_tel"
+ *        in: "formData"
+ *        description: "전화번호"
+ *        required: true
+ *        type: "string"
+ *      - name : "mb_phone"
+ *        in: "formData"
+ *        description: "핸드폰번호"
+ *        required: false
+ *        type: "string"
+ *      - name : "mb_addr1"
+ *        in: "formData"
+ *        description: "주소1"
+ *        required: true
+ *        type: "string"
+ *      - name : "mb_addr2"
+ *        in: "formData"
+ *        description: "주소2"
+ *        required: true
+ *        type: "string"
+ *      responses:
+ *          "200":
+ *              description: "successful operation"
+ *          "400":
+ *              description: "error"
+ */
+router.post(
+    '/register',
+    [
+        body('mb_id', '아이디 입력하세요').notEmpty(),
+        body('mb_password', '패스워드 입력하세요').notEmpty(),
+        body('mb_email', '이메일 올바르지않습니다.').isEmail().notEmpty(),
+        body('mb_phone', '핸드폰 형식이 올바르지않습니다.').isMobilePhone().notEmpty(),
+        body('mb_addr1', '주소1을 입력하세요').notEmpty(),
+        body('mb_addr2', '주소2을 입력하세요').notEmpty()
+    ]
+    , async (req, res, next) => {
+        console.log('=====req1=====');
+        const errors = validationResult(req).errors //에러 배열에 접근
+        if (Object.keys(errors).length !== 0) {
+            let msg = errors.map((item) => {
+                return { item: item.param, msg: item.msg }
+            });
+            const jsonData = commonModule.toJsonData('error', '에러입니다.', msg);
+            res.status(400).json(jsonData);
+        }
+        next();
+    }
+    , async (req, res, next) => {
+    
+        
+    console.log('=====req2=====');
+    console.log(req.body);
+    
+    
+    console.log('hello2222');
+    const jsonData = commonModule.toJsonData('success', '회원 가입 성공1111');
+    res.send(jsonData);
+});
 /**
  * @swagger
  * /api/user/login:
