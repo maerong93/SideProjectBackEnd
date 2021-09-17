6adb99dabfc62f33b0c622304d78a5e2e0e055ec
IBST-yoonSangHyeon <47581425+IBST-yoonSangHyeon@users.noreply.github.com>
2021-09-17 20:54:57 +0900

    임시 저장

--- routes/api/item.js
+++ routes/api/item.js
@@ -6,10 +6,14 @@ const poolConnection = require('../../lib/mysql2Pool');
 const middelwareMember = require('../../middlewares/middlewareMember');
 const { verifyToken } = require('../../middlewares/middlewaresJWT');
 require('dotenv').config();
+const { body, validationResult, check, expressValidator } = require('express-validator'); // 유효성 검사
 const multer = require('multer');
 const itemPath = process.env.FILE_ITEM_PATH;
 const { storageOption, fileFilter } = require('../../lib/multerOption');
-const upload = multer({ storage: storageOption(itemPath) , fileFilter : fileFilter});
+const upload = multer({ storage: storageOption(itemPath), fileFilter: fileFilter });
+const requestIp = require('request-ip');
+const date = require('date-and-time');
+const nowDateTime = date.format(new Date(), 'YYYY-MM-DD hh:mm:ss');
 
 /**
  * @swagger
@@ -101,11 +105,47 @@ router.get('/:it_id'
  *      produces: 
  *      - "application/json"
  *      parameters:
+ *      - name : "it_name"
+ *        in : "formData"
+ *        description: "상품명" 
+ *        required : true
+ *        type: "string"
+ *      - name : "it_cnt"
+ *        in : "formData"
+ *        description: "수량" 
+ *        required : true
+ *        type: "integer"  
+ *      - name : "it_info"
+ *        in : "formData"
+ *        description: "상품 정보" 
+ *        required : true
+ *        type: "string" 
+ *      - name : "it_price"
+ *        in : "formData"
+ *        description: "상품 가격" 
+ *        required : true
+ *        type: "integer"  
+ *      - name : "it_use"
+ *        in : "formData"
+ *        description: "사용 여부 (Y : 사용, N : 미사용)" 
+ *        required : true
+ *        type: "string"  
+ *        enum: [ "Y", "N"]
  *      - name : "it_main_img"
  *        in: "formData"
  *        description: "메인 파일"
  *        required : true
- *        type : file
+ *        type : "file"
+ *      - name : "mb_id"
+ *        in : "formData"
+ *        description: "회원아이디" 
+ *        required : true
+ *        type: "string" 
+ *      - name : "mb_name"
+ *        in : "formData"
+ *        description: "회원명" 
+ *        required : true
+ *        type: "string" 
  *      responses:
  *          "200":
  *              description: "successful operation"      
@@ -113,23 +153,80 @@ router.get('/:it_id'
  *              description: "error"  
  */
 router.post('/'
+    , [
+        body('it_name', '상품명을 입력하세요').notEmpty(),
+        body('it_cnt', '수량의 값을 올바르게 입력해주세요.').isNumeric().isLength({ min: 0 }),
+        body('it_info', '상품정보 5글자에서 255이내로 입력해주세요.').isLength({ min: 5, max:255 }),
+        body('it_price', '상품값을 올바르게 입력해주세요.').isNumeric().isLength({ min: 0 }),
+        body('it_use', '사용여부를 선택해주세요.').isIn(['Y', 'N']),
+        //body('it_main_img', '메인 이미지를 업로드 해주세요.').notEmpty(),
+        body('mb_id', '아이디를 입력하세요.').notEmpty(),
+        body('mb_name', '회원 명을 입력하세요.').notEmpty(),
+    ]
     , async (req, res, next) => {
-        upload.single('it_main_img')(req, res, function (err) {
-            if (err instanceof multer.MulterError) {
-                const jsonData = commonModule.toJsonData('error', '파일 업로드 에러야~~~!!! 빵꾸 똥꾸~~~~', [err]);
+        middelwareMember.checkMember2(req.body.mb_id).then((row) => {
+            console.log('/info');
+            if (row.cnt === 0) {
+                const jsonData = commonModule.toJsonData('error', '등록된 회원이 아닙니다.', []);
                 return res.status(400).json(jsonData);
-            } else if (err) {
-                const jsonData = commonModule.toJsonData('error', req.fileValidationError, [err]);
-                return res.status(400).json(jsonData);
-            } else {
-                return next();
             }
+            return next();
         })
+        
     }
+    // , async (req, res, next) => {
+    //     upload.single('it_main_img')(req, res, function (err) {
+    //         if (err instanceof multer.MulterError) {
+    //             const jsonData = commonModule.toJsonData('error', '파일 업로드 에러야~~~!!! 빵꾸 똥꾸~~~~', [err]);
+    //             return res.status(400).json(jsonData);
+    //         } else if (err) {
+    //             const jsonData = commonModule.toJsonData('error', req.fileValidationError, [err]);
+    //             return res.status(400).json(jsonData);
+    //         } else {
+    //             return next();
+    //         }
+    //     })
+    // }
     , async (req, res, next) => {
-        console.log('hello~~~');
-        console.log(req.file);
-        return res.status(200).json({ msg: 'test' });
+        // try {
+        // let sql = `
+        // INSERT INTO itemd
+        // ( it_name , it_cnt, it_info , it_price ,
+        //   it_use  , it_main_img     , mb_id    , mb_name,
+        //   in_datetime
+        // )
+        // VALUES
+        // ( ?, ?,
+        //   ?, ?,
+        //   ?, ?,
+        //   ?, ?)
+        // `;
+        //     console.log(req.body);
+            
+        // // let values = [
+        // //     req.body.it_name, req.body.it_cnt,
+        // //     req.body.it_info, req.body.it_price,
+        // //     req.body.it_use, 'hello',
+        // //     req.body.mb_id, req.body.mb_name,
+        // //     nowDateTime
+        // // ];
+        
+        // let values = [
+        //     '1', '1',
+        //     '1', '1',
+        //     '1', '1',
+        //     '1', '1'
+        // ];
+        // await poolConnection.query(sql, values);
+        // console.log('success!');
+        // } catch (err) {
+        //     const jsonData = commonModule.toJsonData('error', '쿼리 에러', [err]);
+        //     console.log('에러 발생');
+        //     res.status(400).json(jsonData);
+        //     throw err;
+        // }
+        const jsonData = commonModule.toJsonData('success', '상폼 저장되었습니다.');
+        return res.json(jsonData);
 });
 
 /**
