fa74def980257aa943065b765e8b99a9b78ab09f
IBST-yoonSangHyeon <dalpework@gmail.com>
2021-09-29 15:31:54 +0900

    미들웨어 회원가임 유효성검사 처리
    이메일, 핸드폰번호 유효성 검사 처리함

--- src/routes/user-router.js
+++ src/routes/user-router.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/user-controller');
 const auth = require('../middlewares/auth');
+const validator = require('../middlewares/user-validator');
 
 router.post('/login', UserController.loginUser);
-router.post('/register', UserController.register);
-router.post('/info', auth.auth, UserController.getUser); 
+router.post('/register', validator.register.conditional(), validator.register.func , UserController.register);
+router.post('/info', auth.auth, UserController.getUser);
 
 
 
