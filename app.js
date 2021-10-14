5c061a346354ef67ec36815e1c611ff992ce85b1
IBST-yoonSangHyeon <dalpework@gmail.com>
2021-10-14 16:10:16 +0900

    긴급 app static 추가함 (url 이미지 경로 처리)

--- app.js
+++ app.js
@@ -50,6 +50,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/data/item', express.static(path.join(__dirname, 'data/item'))); 
 
 // fileUrl 설정 (ㅡ.ㅡ URL 이쁘게 구해주는건 없나???? ...)
 app.all("*", (req, res, next) => {
